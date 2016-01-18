80138649802dbbc845c9ad99ed219d0a874025df
Chris Pierce <cpierce@csdurant.com>
2016-01-17 20:05:03 -0600

    small change to make stop creating folder in root dir

--- scripts/rdstos3backup.sh
+++ scripts/rdstos3backup.sh
@@ -2,7 +2,6 @@
 BACKUP_DATE=$(date +"%Y-%m-%d")
 BACKUP_OPTIONS="--rr"
 BACKUP_S3PATH='s3://bucketname/folder'
-mkdir -p "$BACKUP_TMP_PATH"/"$BACKUP_DATE"
 for db in $(mysql -N -s -r -e 'show databases' | grep -v information_schema | grep -v performance_schema | grep -v mysql | grep -v innodb); do
 	echo "Creating DB Backup: $db"
 	/usr/bin/mysqldump "$db" | /bin/gzip | /usr/bin/s3cmd "$BACKUP_OPTIONS" put - "$BACKUP_S3PATH"/"$BACKUP_DATE"/"$db".sql.gz >/dev/null 2>&1
